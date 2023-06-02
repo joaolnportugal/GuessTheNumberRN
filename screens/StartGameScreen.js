bf27d5b2211ac7408a19b1412255f3072db2462e
TUGZ_PC\joaol <joaolnportugal95@gmail.com>
2023-06-02 13:11:16 +0100

    first class

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      //TODO Buttons will go here
+    </View>
+  );
+}
+
+export default StartGameScreen;

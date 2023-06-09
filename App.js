6c18d9e3f76fc2ffa9f54b158f99efe00123d3b8
TUGZ_PC\joaol <joaolnportugal95@gmail.com>
2023-06-09 12:30:03 +0100

    quick commit

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ddb52f",
   },
 });
